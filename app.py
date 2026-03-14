5ca8d6914dd6be4983d0b374cf6d64b7f9e2590a
AjayRaju-18 <ajayraju.m.j2@gamil.com>
2026-03-14 19:50:57 +0530

    started  this simple app

---
+++ app.py
@@ -0,0 +1,13 @@
+
+# Simple web app using Flask
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_file('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
